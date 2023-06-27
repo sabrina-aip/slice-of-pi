7d0e5f7c53eb498b758f9a17525fa62d2b4ef409
sabrina-aip <93457861+sabrina-aip@users.noreply.github.com>
2023-06-27 10:15:01 -0400

    added tool tip
    just to describe what typo forgiveness / mercy mode it. i forgot what i even called it.

--- scripts/detailBreakdown.js
+++ scripts/detailBreakdown.js
@@ -2,9 +2,18 @@ const places = Number(sessionStorage.getItem('places'))
 const headStart = Number(sessionStorage.getItem('headStart'))
 const error = sessionStorage.getItem('error')
 const eb = document.querySelector('.error-breakdown')
-output = []
+let headStart_bool;
+let output = []
 
-output.push(`<p><span id="head-start-entry">3.</span><span id="head-start-entry">${pi.slice(0,headStart)}</span><span id="correct-entry">${pi.slice(headStart,headStart+places)}</span><span id="wrong">${error}</span></p>`)
+console.log(headStart == 0)
+
+if (headStart == 0) {
+  headstart_bool = ``
+} else {
+  headstart_bool = `id="head-start-entry"`
+}
+
+output.push(`<p><span ${headstart_bool}>3.</span><span id="head-start-entry">${pi.slice(0,headStart)}</span><span id="correct-entry">${pi.slice(headStart,headStart+places)}</span><span id="wrong">${error}</span></p>`)
 output.push(`<p>you entered ${error} instead of ${pi[places+headStart]}</p>`)
 
 eb.innerHTML = output.join('');
--- scripts/start.js
+++ scripts/start.js
@@ -1,11 +1,51 @@
+
+// Get starting decimal place input
 const startingPoint_sel = document.querySelector('#startingPoint');
+
+// Get start button
 const startBtn_sel = document.querySelector('#start-btn');
+
+// Get typo mercy checkbox
 const mercy_sel = document.querySelector('#mercy');
-startBtn_sel.addEventListener("click", setStart,{once:true});
 
+// Get the modal
+const modal = document.getElementById("typo-modal");
 
+// Get the button that opens the modal
+const btn = document.getElementById("modal-btn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// initialize variables for setting data
 let startingPoint;
 let mercy;
+
+// When user clicks start button, store the setting data and carry it to the quiz page
+startBtn_sel.addEventListener("click", setStart,{once:true});
+
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", function() {
+  modal.style.display = "block";
+  document.body.style.backgroundColor= "rgba(0,0,0,.4)";
+  startingPoint_sel.style.backgroundColor = "rgba(255,255,255,.4)";
+})
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", function(){
+  modal.style.display = "none";
+  document.body.style.backgroundColor = "white";
+  startingPoint_sel.style.backgroundColor = "rgba(255,255,255,1)";
+})
+
+// When the user clicks anywhere outside of the modal, close it
+document.body.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.body.style.backgroundColor = "white";
+    startingPoint_sel.style.backgroundColor = "rgba(255,255,255,1)";
+  }
+} 
 
 function setStart(e){
   startingPoint = startingPoint_sel.value;
